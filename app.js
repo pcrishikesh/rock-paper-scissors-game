9fc3cecafd8bf062d6565e37d355c7cbd17b7349
Rishikesh P C <hunterpckgh@gmail.com>
2023-05-22 23:01:10 +0530

    updated the readme file

--- app.js
+++ app.js
@@ -5,21 +5,25 @@ let playerSelectionDOM = document.querySelector('.player')
 let computerSelectionDOM = document.querySelector('.computer')
 
 
-
 let btn1 = document.querySelector('.btn1').addEventListener('click',(e)=> {
-    playerSelection=e.target.innerHTML
-    playerSelectionDOM.innerHTML = playerSelection
+    playerSelectionDOM.innerHTML=e.target.innerHTML
 })
 
 
 let btn2 = document.querySelector('.btn2').addEventListener('click',(e)=> {
-    playerSelection=e.target.innerHTML
-    playerSelectionDOM.innerHTML = playerSelection
+    playerSelectionDOM.innerHTML=e.target.innerHTML
 })
 
 
 let btn3 = document.querySelector('.btn3').addEventListener('click',(e)=> {
-    playerSelection=e.target.innerHTML
-    playerSelectionDOM.innerHTML = playerSelection
+    playerSelectionDOM.innerHTML=e.target.innerHTML
 })
 
+function gameRound() {
+
+    if (yes) {
+        console.log("yes");
+    }else{console.log('no');}
+}
+
+gameRound()
