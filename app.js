52331d1c5f27d44bfaaec4b64ff6b7652499029d
Rishikesh P C <hunterpckgh@gmail.com>
2023-07-03 23:51:53 +0530

    ui updated

--- app.js
+++ app.js
@@ -4,26 +4,43 @@
 let playerSelectionDOM = document.querySelector('.playerSelection')
 let computerSelectionDOM = document.querySelector('.computer')
 
+let selection = ["","",""]
+
+main()
+
+function main() {
+    onclickChange()
+}
+
+function onclickChange() {
+    document.querySelector('.btn1').addEventListener('click',(e)=> {
+        selection[0]="stone"
+        playerSelectionDOM.innerHTML = selection[0]
+    })
+    
+    
+    document.querySelector('.btn2').addEventListener('click',(e)=> {
+        selection[1]="paper"
+        playerSelectionDOM.innerHTML = selection[1]
+    
+    })
+    
+    
+    document.querySelector('.btn3').addEventListener('click',(e)=> {
+        selection[2]="scissors"
+        playerSelectionDOM.innerHTML = selection[2]
+    
+    })
 
 
-let btn1 = document.querySelector('.btn1').addEventListener('click',(e)=> {
-    selection="stone"
-    playerSelectionDOM.innerHTML = selection
-})
+}
 
 
-let btn2 = document.querySelector('.btn2').addEventListener('click',(e)=> {
-    selection="paper"
-    playerSelectionDOM.innerHTML = selection
-
-})
-
-
-let btn3 = document.querySelector('.btn3').addEventListener('click',(e)=> {
-    selection="scissors"
-    playerSelectionDOM.innerHTML = selection
-
-})
-
-
-gameRound()
+for (i=0;i<selection.length;i++) {
+    selection[i] = "hi"
+    if (selection[i] == "hi") {
+        console.log('yes');
+    }else {
+        console.log('no');
+    }
+}
