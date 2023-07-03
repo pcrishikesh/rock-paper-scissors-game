b96ea1f58ce0f7c292a02b90523b5b3efe7cc3ba
Rishikesh P C <hunterpckgh@gmail.com>
2023-07-03 23:54:52 +0530

    rewriting the code

--- app.js
+++ app.js
@@ -1,29 +1,2 @@
 // rock paper scissors game
 
-
-let playerSelectionDOM = document.querySelector('.player')
-let computerSelectionDOM = document.querySelector('.computer')
-
-
-let btn1 = document.querySelector('.btn1').addEventListener('click',(e)=> {
-    playerSelectionDOM.innerHTML=e.target.innerHTML
-})
-
-
-let btn2 = document.querySelector('.btn2').addEventListener('click',(e)=> {
-    playerSelectionDOM.innerHTML=e.target.innerHTML
-})
-
-
-let btn3 = document.querySelector('.btn3').addEventListener('click',(e)=> {
-    playerSelectionDOM.innerHTML=e.target.innerHTML
-})
-
-function gameRound() {
-
-    if (yes) {
-        console.log("yes");
-    }else{console.log('no');}
-}
-
-gameRound()
